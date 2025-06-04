8ee3d2b9644d44fb30e9671c2bd7ebf7289bbd54
HamzaElabboubi <elabboubihamza8@gmail.com>
2025-06-04 10:14:14 +0100

    correction des importation

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { initThemeMode } from "flowbite-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import ExcelComparer from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ExcelComparer />
   </StrictMode>,
 );
 
